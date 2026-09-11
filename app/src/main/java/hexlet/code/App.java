737eb486b39e2e0979bf962a76f7f8a323f087b9
Vasyl L Polyak <frvpolyak@gmail.com>
2026-09-11 18:00:08 -0400

    Extract URL handlers into controller

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,22 +5,13 @@ import com.zaxxer.hikari.HikariDataSource;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import gg.jte.resolve.ResourceCodeResolver;
-import hexlet.code.model.Url;
-import hexlet.code.model.UrlCheck;
+import hexlet.code.controller.UrlController;
 import hexlet.code.repository.BaseRepository;
-import hexlet.code.repository.UrlCheckRepository;
-import hexlet.code.repository.UrlRepository;
 import io.javalin.Javalin;
-import io.javalin.http.Context;
 import io.javalin.http.staticfiles.Location;
 import io.javalin.rendering.template.JavalinJte;
-import kong.unirest.core.Unirest;
-import org.jsoup.Jsoup;
 
-import java.net.URI;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 public class App {
 
@@ -38,25 +29,10 @@ public class App {
     }
 
     private static TemplateEngine createTemplateEngine() {
-        ClassLoader classLoader = App.class.getClassLoader();
+        var classLoader = App.class.getClassLoader();
+        var codeResolver = new ResourceCodeResolver("templates", classLoader);
 
-        ResourceCodeResolver codeResolver =
-                new ResourceCodeResolver("templates", classLoader);
-
-        return TemplateEngine.create(
-                codeResolver,
-                ContentType.Html
-        );
-    }
-
-    private static String getFlash(Context ctx) {
-        String flash = ctx.sessionAttribute("flash");
-
-        if (flash != null) {
-            ctx.sessionAttribute("flash", null);
-        }
-
-        return flash;
+        return TemplateEngine.create(codeResolver, ContentType.Html);
     }
 
     public static Javalin getApp() {
@@ -81,247 +57,22 @@ public class App {
         return Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
 
-            config.staticFiles.add(
-                    "/static",
-                    Location.CLASSPATH
-            );
+            config.staticFiles.add("/static", Location.CLASSPATH);
 
             config.fileRenderer(
                     new JavalinJte(createTemplateEngine())
             );
 
-            config.routes.get("/", ctx -> {
-                var flash = getFlash(ctx);
-
-                ctx.render(
-                        "index.jte",
-                        Map.of(
-                                "flash",
-                                flash == null ? "" : flash
-                        )
-                );
-            });
-
-            config.routes.get("/urls", ctx -> {
-                var urls = UrlRepository.getEntities();
-                var latestChecks = new HashMap<Long, UrlCheck>();
-
-                for (var url : urls) {
-                    var latestCheck =
-                            UrlCheckRepository.findLatestByUrlId(
-                                    url.getId()
-                            );
-
-                    latestCheck.ifPresent(
-                            check -> latestChecks.put(
-                                    url.getId(),
-                                    check
-                            )
-                    );
-                }
-
-                var flash = getFlash(ctx);
-
-                ctx.render(
-                        "urls/index.jte",
-                        Map.of(
-                                "urls", urls,
-                                "latestChecks", latestChecks,
-                                "flash", flash == null ? "" : flash
-                        )
-                );
-            });
-
-            config.routes.post("/urls", ctx -> {
-                var url = ctx.formParam("url");
-
-                try {
-                    var uri = new URI(url);
-
-                    var scheme = uri.getScheme();
-                    var host = uri.getHost();
-
-                    if (scheme == null
-                            || (!scheme.equals("http")
-                            && !scheme.equals("https"))
-                            || host == null) {
-
-                        ctx.status(422);
-
-                        ctx.render(
-                                "index.jte",
-                                Map.of(
-                                        "flash",
-                                        "Некорректный URL"
-                                )
-                        );
-
-                        return;
-                    }
-
-                    var normalizedUrl = new URI(
-                            scheme,
-                            null,
-                            host,
-                            uri.getPort(),
-                            null,
-                            null,
-                            null
-                    ).toString();
-
-                    var existingUrl =
-                            UrlRepository.findByName(normalizedUrl);
-
-                    if (existingUrl.isPresent()) {
-                        ctx.sessionAttribute(
-                                "flash",
-                                "Страница уже существует"
-                        );
-
-                        ctx.redirect(
-                                "/urls/" + existingUrl.get().getId()
-                        );
-
-                        return;
-                    }
-
-                    var newUrl = new Url(normalizedUrl);
-                    UrlRepository.save(newUrl);
-
-                    var savedUrl = UrlRepository
-                            .findByName(normalizedUrl)
-                            .orElseThrow();
-
-                    ctx.sessionAttribute(
-                            "flash",
-                            "Страница успешно добавлена"
-                    );
-
-                    ctx.redirect(
-                            "/urls/" + savedUrl.getId()
-                    );
-
-                } catch (Exception e) {
-                    ctx.status(422);
-
-                    ctx.render(
-                            "index.jte",
-                            Map.of(
-                                    "flash",
-                                    "Некорректный URL"
-                            )
-                    );
-                }
-            });
-
-            config.routes.get("/urls/{id}", ctx -> {
-                var id = Long.parseLong(
-                        ctx.pathParam("id")
-                );
-
-                var url = UrlRepository
-                        .find(id)
-                        .orElseThrow();
-
-                var checks =
-                        UrlCheckRepository.getByUrlId(id);
-
-                var flash = getFlash(ctx);
-
-                ctx.render(
-                        "urls/show.jte",
-                        Map.of(
-                                "url", url,
-                                "checks", checks,
-                                "flash", flash == null ? "" : flash
-                        )
-                );
-            });
-
-            config.routes.post("/urls/{id}/checks", ctx -> {
-                var id = Long.parseLong(
-                        ctx.pathParam("id")
-                );
-
-                var url = UrlRepository
-                        .find(id)
-                        .orElseThrow();
-
-                try {
-                    var response = Unirest
-                            .get(url.getName())
-                            .asString();
-
-                    var statusCode = response.getStatus();
-
-                    if (statusCode >= 400) {
-                        ctx.sessionAttribute(
-                                "flash",
-                                "Произошла ошибка при проверке"
-                        );
-
-                        ctx.redirect("/urls/" + id);
-                        return;
-                    }
-
-                    var document = Jsoup.parse(
-                            response.getBody()
-                    );
-
-                    var title = document.title();
-
-                    var h1Element =
-                            document.selectFirst("h1");
-
-                    var h1 = h1Element == null
-                            ? null
-                            : h1Element.text();
-
-                    var descriptionElement =
-                            document.selectFirst(
-                                    "meta[name=description]"
-                            );
-
-                    var description =
-                            descriptionElement == null
-                                    ? null
-                                    : descriptionElement.attr(
-                                    "content"
-                            );
-
-                    var check = new UrlCheck(
-                            id,
-                            statusCode
-                    );
-
-                    check.setTitle(title);
-                    check.setH1(h1);
-                    check.setDescription(description);
-
-                    UrlCheckRepository.save(check);
-
-                    ctx.sessionAttribute(
-                            "flash",
-                            "Страница успешно проверена"
-                    );
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-
-                    ctx.sessionAttribute(
-                            "flash",
-                            "Произошла ошибка при проверке"
-                    );
-                }
-
-                ctx.redirect("/urls/" + id);
-            });
+            config.routes.get("/", UrlController::home);
+            config.routes.get("/urls", UrlController::index);
+            config.routes.post("/urls", UrlController::create);
+            config.routes.get("/urls/{id}", UrlController::show);
+            config.routes.post("/urls/{id}/checks", UrlController::check);
         });
     }
 
     public static void main(String[] args) {
         var app = getApp();
-
         var port = System.getenv("PORT");
 
         if (port != null) {
---
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -0,0 +1,211 @@
+package hexlet.code.controller;
+
+import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+import hexlet.code.repository.UrlCheckRepository;
+import hexlet.code.repository.UrlRepository;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import kong.unirest.core.Unirest;
+import org.jsoup.Jsoup;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UrlController {
+
+    private static String getFlash(Context ctx) {
+        String flash = ctx.sessionAttribute("flash");
+
+        if (flash != null) {
+            ctx.sessionAttribute("flash", null);
+        }
+
+        return flash;
+    }
+
+    public static void home(Context ctx) {
+        var flash = getFlash(ctx);
+
+        ctx.render(
+                "index.jte",
+                Map.of("flash", flash == null ? "" : flash)
+        );
+    }
+
+    public static void index(Context ctx) throws Exception {
+        var urls = UrlRepository.getEntities();
+        var latestChecks = new HashMap<Long, UrlCheck>();
+
+        for (var url : urls) {
+            var latestCheck = UrlCheckRepository.findLatestByUrlId(url.getId());
+
+            latestCheck.ifPresent(
+                    check -> latestChecks.put(url.getId(), check)
+            );
+        }
+
+        var flash = getFlash(ctx);
+
+        ctx.render(
+                "urls/index.jte",
+                Map.of(
+                        "urls", urls,
+                        "latestChecks", latestChecks,
+                        "flash", flash == null ? "" : flash
+                )
+        );
+    }
+
+    public static void create(Context ctx) throws Exception {
+        var inputUrl = ctx.formParam("url");
+        URI uri;
+
+        try {
+            uri = new URI(inputUrl);
+        } catch (Exception e) {
+            renderInvalidUrl(ctx);
+            return;
+        }
+
+        var scheme = uri.getScheme();
+        var host = uri.getHost();
+
+        if (scheme == null
+                || (!scheme.equals("http") && !scheme.equals("https"))
+                || host == null) {
+            renderInvalidUrl(ctx);
+            return;
+        }
+
+        var normalizedUrl = new URI(
+                scheme,
+                null,
+                host,
+                uri.getPort(),
+                null,
+                null,
+                null
+        ).toString();
+
+        var existingUrl = UrlRepository.findByName(normalizedUrl);
+
+        if (existingUrl.isPresent()) {
+            ctx.sessionAttribute("flash", "Страница уже существует");
+            ctx.redirect("/urls/" + existingUrl.get().getId());
+            return;
+        }
+
+        var newUrl = new Url(normalizedUrl);
+        UrlRepository.save(newUrl);
+
+        var savedUrl = UrlRepository
+                .findByName(normalizedUrl)
+                .orElseThrow();
+
+        ctx.sessionAttribute(
+                "flash",
+                "Страница успешно добавлена"
+        );
+
+        ctx.redirect("/urls/" + savedUrl.getId());
+    }
+
+    private static void renderInvalidUrl(Context ctx) {
+        ctx.status(HttpStatus.UNPROCESSABLE_CONTENT);
+
+        ctx.render(
+                "index.jte",
+                Map.of(
+                        "flash",
+                        "Некорректный URL"
+                )
+        );
+    }
+
+    public static void show(Context ctx) throws Exception {
+        var id = Long.parseLong(ctx.pathParam("id"));
+
+        var url = UrlRepository
+                .find(id)
+                .orElseThrow();
+
+        var checks = UrlCheckRepository.getByUrlId(id);
+        var flash = getFlash(ctx);
+
+        ctx.render(
+                "urls/show.jte",
+                Map.of(
+                        "url", url,
+                        "checks", checks,
+                        "flash", flash == null ? "" : flash
+                )
+        );
+    }
+
+    public static void check(Context ctx) throws Exception {
+        var id = Long.parseLong(ctx.pathParam("id"));
+
+        var url = UrlRepository
+                .find(id)
+                .orElseThrow();
+
+        try {
+            var response = Unirest
+                    .get(url.getName())
+                    .asString();
+
+            var statusCode = response.getStatus();
+
+            if (statusCode >= HttpStatus.BAD_REQUEST.getCode()) {
+                ctx.sessionAttribute(
+                        "flash",
+                        "Произошла ошибка при проверке"
+                );
+
+                ctx.redirect("/urls/" + id);
+                return;
+            }
+
+            var document = Jsoup.parse(response.getBody());
+            var title = document.title();
+
+            var h1Element = document.selectFirst("h1");
+            var h1 = h1Element == null
+                    ? null
+                    : h1Element.text();
+
+            var descriptionElement = document.selectFirst(
+                    "meta[name=description]"
+            );
+
+            var description = descriptionElement == null
+                    ? null
+                    : descriptionElement.attr("content");
+
+            var check = new UrlCheck(id, statusCode);
+
+            check.setTitle(title);
+            check.setH1(h1);
+            check.setDescription(description);
+
+            UrlCheckRepository.save(check);
+
+            ctx.sessionAttribute(
+                    "flash",
+                    "Страница успешно проверена"
+            );
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            ctx.sessionAttribute(
+                    "flash",
+                    "Произошла ошибка при проверке"
+            );
+        }
+
+        ctx.redirect("/urls/" + id);
+    }
+}
